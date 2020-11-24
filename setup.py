be099f71cefe6d23a517157aaca0932e30137a22
ozz <nosudafix@larjem.com>
2020-11-23 20:42:06 -0500

    rm xps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="browserhist",
-    version="0.4.0",
+    version="0.4.1",
     author="Ashoka Lella",
     packages=find_packages(),
     install_requires=[
